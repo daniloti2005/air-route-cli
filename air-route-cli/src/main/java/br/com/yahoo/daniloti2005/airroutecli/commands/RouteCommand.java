40492e40a7b3d57a013b10a6d30d330d71c4e9fb
daniloti2005 <daniloti2005@yahoo.combr>
2021-01-27 11:35:07 -0300

    new library

---
+++ air-route-cli/src/main/java/br/com/yahoo/daniloti2005/airroutecli/commands/RouteCommand.java
@@ -0,0 +1,8 @@
+package br.com.yahoo.daniloti2005.airroutecli.commands;
+
+import br.com.daniloti2005.air_route_commons.service.csv.*;
+import br.com.yahoo.daniloti2005.air;
+
+public class RouteCommand {
+    
+}
